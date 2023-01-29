1c806c17b558a47a70da6074fcf7d450c1d976b3
artem karapetyan <artemka111777@mail.ru>
2023-01-29 17:02:59 +0300

    3. mysql book manager: initial

---
+++ book-management/pkg/controller/book-controllers.go
@@ -0,0 +1,38 @@
+package controller
+
+import "net/http"
+
+func GetBooks() http.HandlerFunc  {
+
+	return func(w http.ResponseWriter, r *http.Request ) {
+
+	}
+}
+
+func CreateBook() http.HandlerFunc  {
+
+	return func(w http.ResponseWriter, r *http.Request ) {
+		
+	}
+}
+
+func GetBookByID() http.HandlerFunc  {
+
+	return func(w http.ResponseWriter, r *http.Request ) {
+		
+	}
+}
+
+func UpdateBook() http.HandlerFunc  {
+
+	return func(w http.ResponseWriter, r *http.Request ) {
+		
+	}
+}
+
+func DeleteBook() http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request ) {
+		
+	}
+}
---
+++ book-management/pkg/routes/bookstore-routes.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	ctrl "github.com/artemxgod/11-go-projects/book-management/pkg/controller"
+	"github.com/gorilla/mux"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", ctrl.GetBooks()).Methods("POST")
+	router.HandleFunc("/book/", ctrl.CreateBook()).Methods("GET")
+	router.HandleFunc("/book/{id}", ctrl.GetBookByID()).Methods("GET")
+	router.HandleFunc("/book/{id}", ctrl.UpdateBook()).Methods("PUT")
+	router.HandleFunc("/book/{id}", ctrl.DeleteBook()).Methods("DELETE")
+
+}
