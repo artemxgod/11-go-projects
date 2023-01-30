91bae58e44dcd86c2faa89381243e4e2a70a0d03
artem karapetyan <artemka111777@mail.ru>
2023-01-30 18:48:09 +0300

    update readme and add sourse code

--- email-verifier/README.md
+++ email-verifier/README.md
@@ -1,3 +1,16 @@
 # Email verifier tool
 
 ## Layout
+
+* Contains main application for this project
+
+* The name of executable should match the directory name of application in this case it is `verifier`
+
+* main.go invokes checker from `/pkg`
+
+
+## What it does
+
+* Not much. It checks domain for MX, SPF and DMARC. 
+
+* As an additional task i also add email validation
--- email-verifier/pkg/checker/checker.go
+++ email-verifier/pkg/checker/checker.go
@@ -3,10 +3,12 @@ package checker
 import (
 	"bufio"
 	"fmt"
+	"net"
 	"os"
+	"strings"
+
 	validation "github.com/go-ozzo/ozzo-validation"
 	"github.com/go-ozzo/ozzo-validation/is"
-
 )
 
 func StartCheck() error {
@@ -31,10 +33,49 @@ func checkDomain(email string) error {
 		return nil
 	}
 
+	_, domain, _ := strings.Cut(email, "@")
+
 	var hasMX, hasSPF, hasDMARC bool
 	var spfRecord, dmarcRecord string
 
+	mxRecords, err := net.LookupMX(domain)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if len(mxRecords) > 0 {
+		hasMX = true
+	}
+
+	txtRecords, err := net.LookupTXT(domain)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	hasSPF, spfRecord = findRecord(txtRecords, "v=spf1")
+
+	dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	hasDMARC, dmarcRecord = findRecord(dmarcRecords, "v=DMARC1")
+
+
+	fmt.Printf("- Has MX: %v\n- Has SPF: %v\n" + 
+		"- SPF record: %s\n- Has DMARC: %v\n- DMARC record: %s\n", hasMX, hasSPF, spfRecord, hasDMARC, dmarcRecord)
+
 	return nil
+}
+
+func findRecord(records []string, prefix string) (bool, string) {
+	for _, record := range records {
+		if strings.HasPrefix(record, prefix) {
+			return true, record
+		}
+	}
+
+	return false, ""
 }
 
 func ValidateEmail(email string) error {
