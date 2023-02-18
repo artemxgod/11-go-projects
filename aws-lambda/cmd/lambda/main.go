57d09573ca88bd19a785ca725bbdea7e964cf58f
artem karapetyan <artemka111777@mail.ru>
2023-02-19 00:42:54 +0300

    update readme

--- aws-lambda/cmd/lambda/main.go
+++ aws-lambda/cmd/lambda/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type Event struct {
+	Name string `json:"what is your name?"`
+	Age int `json:"how old are you?"`
+}
+
+type Response struct {
+	Message string `json:"Answer:"`
+}
+
 func main() {
-	
+	lambda.Start(HandleLambdaEvent)
+}
+
+func HandleLambdaEvent(event Event) (Response, error) {
+
+	return Response{Message: fmt.Sprintf("%s is %d years old", event.Name, event.Age)}, nil
 }
--- aws-lambda/go.mod
+++ aws-lambda/go.mod
@@ -1,3 +1,5 @@
 module github.com/artemxgod/11-go-projects/aws-lambda
 
 go 1.19
+
+require github.com/aws/aws-lambda-go v1.37.0 // indirect
